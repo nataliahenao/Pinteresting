f337c6dcc1efbe648b519403f1f1e41143fee8f4
alejo <jagomez086@misena.edu.co>
2016-07-25 20:49:26 -0500

    actualizacion pinteresting_version2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #require for heroku
+  #note to set this ti your actual host
+  config.action_mailer.default_url_options = { host: 'https://pinterestingnatalia.herokuapp.com/' }
 end
